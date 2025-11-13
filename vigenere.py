45ab8b4172a650f66517135da172df1848c729a0
LeChicheurFou <antonin.plessis@gmail.com>
2025-11-13 11:25:00 +0100

    Refactor : all functions

--- vigenere.py
+++ vigenere.py
@@ -1,20 +1,17 @@
 import string
 
-def cesar_cipher(text, key):
+def cesar_cipher(text, key, cipher=True):
 	if type(text) == str and type(key) == int:
-		return "".join([chr((ord(char) + key) % 1_114_112) for char in text])
+		return "".join([chr((ord(char) + (1 if cipher else -1)* key) % 1_114_112)\
+				  												for char in text])
 	else:
 		raise(TypeError)
-
-
-def cesar_uncipher(crypted_text, key):
-		return cesar_cipher(crypted_text, -key)
 
 
 def hack_cesar_cipher(crypted_text, alphabet):
 	if type(crypted_text) == str and type(alphabet) == str:
 		for possible_key in range(0, 1_114_112):
-			possible_uncryption = cesar_uncipher(crypted_text, possible_key)
+			possible_uncryption = cesar_cipher(crypted_text, possible_key, cipher=False)
 			if possible_uncryption[0] in alphabet:
 				print(possible_key)
 				print(possible_uncryption)
@@ -23,19 +20,25 @@ def hack_cesar_cipher(crypted_text, alphabet):
 		raise(TypeError)
 
 
-def vigenere_cipher(text, password):
+def vigenere_cipher(text, password, cipher=True):
 	list_of_keys = [ord(char) for char in password]
 	crypted_text = []
 	for index, char in enumerate(text):
 		current_key = list_of_keys[index % len(list_of_keys)]
-		crypted_text.append(cesar_cipher(char , current_key))
+		if cipher:
+			crypted_text.append(cesar_cipher(char , current_key))
+		else:
+			crypted_text.append(cesar_cipher(char , current_key, cipher=False))
 	return "".join(crypted_text)
 
-def vigenere_uncipher(crypted_text , password):
-	list_of_keys = [ord(char) for char in password]
-	decrypted_text = []
-	for index, char in enumerate(crypted_text):
-		current_key = list_of_keys[index % len(list_of_keys)]
-		decrypted_text.append(cesar_uncipher(char , current_key))
-	return "".join(decrypted_text)
 
+# def vigenere_uncipher(crypted_text , password):
+# 	list_of_keys = [ord(char) for char in password]
+# 	decrypted_text = []
+# 	for index, char in enumerate(crypted_text):
+# 		current_key = list_of_keys[index % len(list_of_keys)]
+# 		decrypted_text.append(cesar_uncipher(char , current_key))
+# 	return "".join(decrypted_text)
+
+print(vigenere_cipher("test","b"))
+print(vigenere_cipher("ÖÇÕÖ","b",False))
